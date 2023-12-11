5f68372693d47e86fe6f1ee21334eb4d1f56cbf3
urosjevremovic <jevremovic.uros91@gmail.com>
2023-12-11 23:05:39 +0100

    add all the rest of the plots with values for needed letters. Add nine additional code name games. Make all of them dynamicly refresh upon user making a mistake

--- code_names_dict.json.js
+++ code_names_dict.json.js
@@ -1,58 +1,61 @@
-var data = '[{1: { "main_word" : "Weapon", "words" : {"green1": "Sword", "green2": "Gun","green3": "Knife", '
+var data = JSON.parse('[{"1": { "main_word" : "Weapon", "words" : {"green1": "Sword", "green2": "Gun","green3": "Knife", '
 + '"red1": "Salt", "red2": "River", "red3": "Apple", "red4": "Black", "red5": "Song", "red6": "Bear", "red7": "Engine"}},'
-+'2: { "main_word" : "Winter", "words" : {"green1": "Snow", "green2": "Holiday","green3": "Christmas Tree", '
++'"2": { "main_word" : "Winter", "words" : {"green1": "Snow", "green2": "Holiday","green3": "Christmas", '
 + '"red1": "Blood", "red2": "Glass", "red3": "Ceremony", "red4": "Steam", "red5": "Bread", "red6": "Cat", "red7": "Veteran"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
++'"3": { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
 + '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'4: { "main_word" : "Idea", "words" : {"green1": "Light bulb", "green2": "Eureka","green3": "Thought", '
++'"4": { "main_word" : "Idea", "words" : {"green1": "Light bulb", "green2": "Eureka","green3": "Thought", '
 + '"red1": "Combine", "red2": "Deputy", "red3": "Cash", "red4": "Tidy", "red5": "Worker", "red6": "Addicted", "red7": "Tolerate"}},'
-+'5: { "main_word" : "Life", "words" : {"green1": "Birth", "green2": "Meaning","green3": "Death", '
++'"5": { "main_word" : "Life", "words" : {"green1": "Birth", "green2": "Meaning","green3": "Death", '
 + '"red1": "Text", "red2": "Offend", "red3": "Invite", "red4": "Layout", "red5": "Justify", "red6": "Dictate", "red7": "Theorist"}},'
-+'6: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
++'"6": { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
 + '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'+'3: { "main_word" : "Sun", "words" : {"green1": "Warmth", "green2": "Ray","green3": "Shine", '
-+ '"red1": "Tender", "red2": "User", "red3": "Progress", "red4": "Command", "red5": "Play", "red6": "Fox", "red7": "Tongue"}},'
-}]';
++'"7": { "main_word" : "Animated", "words" : {"green1": "Disney", "green2": "Comic Book","green3": "Movie", '
++ '"red1": "Dangerous", "red2": "Fool", "red3": "Interest", "red4": "Cool", "red5": "Swim", "red6": "Long", "red7": "Reactor"}},'
++'"8": { "main_word" : "Street", "words" : {"green1": "Car", "green2": "Parking","green3": "Fighter", '
++ '"red1": "Rain", "red2": "Dollar", "red3": "Mutual", "red4": "Please", "red5": "Navy", "red6": "Rebel", "red7": "Wind"}},'
++'"9": { "main_word" : "Fruit", "words" : {"green1": "Passion", "green2": "Orange","green3": "Vitamin", '
++ '"red1": "Camp", "red2": "Mask", "red3": "State", "red4": "Native", "red5": "Tube", "red6": "End", "red7": "Blade"}},'
++'"10": { "main_word" : "Health", "words" : {"green1": "Mental", "green2": "Doctor","green3": "Care", '
++ '"red1": "Profile", "red2": "Battery", "red3": "Pen", "red4": "Pleasure", "red5": "Consumer", "red6": "Suppress", "red7": "Liberal"}},'
++'"11": { "main_word" : "Hair", "words" : {"green1": "Shampoo", "green2": "Dresser","green3": "Dryer", '
++ '"red1": "World", "red2": "Disclose", "red3": "Candidate", "red4": "Inspire", "red5": "Release", "red6": "Opponent", "red7": "Fraction"}},'
++'"12": { "main_word" : "Plastic", "words" : {"green1": "Bag", "green2": "Waste","green3": "Bottle", '
++ '"red1": "Amber", "red2": "Journal", "red3": "Learn", "red4": "Important", "red5": "Elite", "red6": "Sheep", "red7": "Alive"}},'
++'"13": { "main_word" : "Building", "words" : {"green1": "House", "green2": "Skyscraper","green3": "Church", '
++ '"red1": "Pick", "red2": "Mayor", "red3": "Remain", "red4": "Coerce", "red5": "Argument", "red6": "Bride", "red7": "Wrong"}},'
++'"14": { "main_word" : "Flower", "words" : {"green1": "Rose", "green2": "Wild","green3": "Child", '
++ '"red1": "Support", "red2": "Soak", "red3": "Slump", "red4": "Expansion", "red5": "Variety", "red6": "Mood", "red7": "Spy"}},'
++'"15": { "main_word" : "Head", "words" : {"green1": "Hoodie", "green2": "Hat","green3": "Beanie", '
++ '"red1": "Shelter", "red2": "Ankle", "red3": "Immune", "red4": "Burial", "red5": "Progress", "red6": "Sodium", "red7": "Cream"}},'
++'"16": { "main_word" : "Match", "words" : {"green1": "Stadium", "green2": "Football","green3": "Derby", '
++ '"red1": "Transport", "red2": "Word", "red3": "Payment", "red4": "Vision", "red5": "String", "red6": "Injury", "red7": "Straight"}},'
++'"17": { "main_word" : "Grain", "words" : {"green1": "Bread", "green2": "Wheat","green3": "Oats", '
++ '"red1": "March", "red2": "Site", "red3": "Narrow", "red4": "Dismissal", "red5": "Blue", "red6": "Heart", "red7": "Grave"}},'
++'"18": { "main_word" : "Cloud", "words" : {"green1": "Rain", "green2": "Storage","green3": "Computing", '
++ '"red1": "Choice", "red2": "Give", "red3": "Spring", "red4": "Ballet", "red5": "Confidence", "red6": "Notebook", "red7": "Knock"}},'
++'"19": { "main_word" : "Cookie", "words" : {"green1": "Jar", "green2": "Chocolate","green3": "Monster", '
++ '"red1": "Articulate", "red2": "Aware", "red3": "Spectrum", "red4": "Period", "red5": "Fame", "red6": "Carriage", "red7": "Pack"}},'
++'"20": { "main_word" : "Jedi", "words" : {"green1": "Republic", "green2": "Saber","green3": "Knight", '
++ '"red1": "Scan", "red2": "Embrace", "red3": "Hike", "red4": "Desk", "red5": "Quality", "red6": "Thrust", "red7": "Like"}},'
++'"21": { "main_word" : "Law", "words" : {"green1": "Order", "green2": "Court","green3": "Attorney", '
++ '"red1": "Mole", "red2": "Palace", "red3": "Cancel", "red4": "Preach", "red5": "Denial", "red6": "Perception", "red7": "Cast"}}}]');
+
+var list_number = JSON.parse('[0,1,2,3,4,5,6,7,8,9]');
+
+var randomProperty = function (obj) {
+  var keys = Object.keys(obj);
+    var index = keys[ keys.length * Math.random() << 0];
+  return_obj = obj[index];
+    delete obj[index];
+    return return_obj;
+};
+
+var randomizeArray = function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
